2572b92ed1eff1d9ef8979ab3313ab1fa0a4d6bf
Eugene Ryabenko <ihuman100@gmail.com>
2023-06-21 23:07:13 +0300

    Add file ourProductsTest.cy.js

---
+++ cypress/e2e/ourProductsTest.cy.js
@@ -0,0 +1,65 @@
+/// <reference types="Cypress" />
+import HomePage from "../pages/HomePage";
+import BasePage from "../pages/BasePage";
+import OurProductsPage from "../pages/OurProductsPage";
+import ContactUsPage from "../pages/ContactUsPage";
+
+const basePage = new BasePage();
+const homePage = new HomePage();
+const ourProductsPage = new OurProductsPage();
+const contactUsPage = new ContactUsPage();
+
+describe('Our Product page tests', () => {
+	beforeEach(() => {
+		homePage.visitHomePage();
+		homePage.clickOurProductsButton();
+	});
+
+	it('Check "Proceed" modal button', () => {
+		for(let i=0; i<8; i++) {
+			ourProductsPage.clickTitles(i);
+			ourProductsPage.getSpecialOfferModal().should('be.visible');
+			ourProductsPage.clickProceedButton();
+			ourProductsPage.getSpecialOfferModal().should('not.be.visible');
+		}
+
+		for(let i=0; i<8; i++) {
+			ourProductsPage.clickImages(i);
+			ourProductsPage.getSpecialOfferModal().should('be.visible');
+			ourProductsPage.clickProceedButton();
+			ourProductsPage.getSpecialOfferModal().should('not.be.visible');
+		}
+	});
+
+	it('Check "Close" modal button', () => {
+		for(let i=0; i<8; i++) {
+			ourProductsPage.clickTitles(i);
+			ourProductsPage.getSpecialOfferModal().should('be.visible');
+			ourProductsPage.clickCloseModalButton();
+			ourProductsPage.getSpecialOfferModal().should('not.be.visible');
+		}
+
+		for(let i=0; i<8; i++) {
+			ourProductsPage.clickImages(i);
+			ourProductsPage.getSpecialOfferModal().should('be.visible');
+			ourProductsPage.clickCloseModalButton();
+			ourProductsPage.getSpecialOfferModal().should('not.be.visible');
+		}
+	});
+
+	it('Check "Exit" modal button', () => {
+		for(let i=0; i<8; i++) {
+			ourProductsPage.clickTitles(i);
+			ourProductsPage.getSpecialOfferModal().should('be.visible');
+			ourProductsPage.clickExitModalButton();
+			ourProductsPage.getSpecialOfferModal().should('not.be.visible');
+		}
+
+		for(let i=0; i<8; i++) {
+			ourProductsPage.clickImages(i);
+			ourProductsPage.getSpecialOfferModal().should('be.visible');
+			ourProductsPage.clickExitModalButton();
+			ourProductsPage.getSpecialOfferModal().should('not.be.visible');
+		}
+	});
+});
