bd105712b9661777b28a053962dc11ba658362c5
Eugene Ryabenko <ihuman100@gmail.com>
2023-06-21 23:15:15 +0300

    Add files errors.js and users.js

---
+++ cypress/test-data/users.js
@@ -0,0 +1,17 @@
+export const users = [{
+	firstName: "Arnold",
+	lastName: "Schwarzenegger",
+	email: "arni@schwarz.com",
+	comment: "Hasta la vista, baby"
+},
+{
+	firstName: "Arnold",
+	lastName: "Schwarzenegger",
+	email: "jackiechan.com",
+	comment: "Hasta la vista, baby"
+},
+{
+	firstName: "Arnold",
+	lasName: "Schwarzenegger",
+	email: "arni@schwarz.com",
+}]
