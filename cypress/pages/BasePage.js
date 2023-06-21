45f51e66899c829a5af150d7fb35c2f03d0a8290
Eugene Ryabenko <ihuman100@gmail.com>
2023-06-21 23:10:14 +0300

    Add file BasePage.js

---
+++ cypress/pages/BasePage.js
@@ -0,0 +1,32 @@
+class BasePage {
+
+	getOurProductsButton() {
+		return cy.get('a[href="products.html"]');
+	}
+
+	getContactUsButton() {
+		return cy.get('a[href="../Contact-Us/contactus.html"]');
+	}
+
+	getHomeButton() {
+		return cy.get('a[href="index.html"]');
+	}
+
+	getFooter() {
+		return cy.get('footer p');
+	}
+
+	clickHomeButton(){
+		this.getHomeButton().click();
+	}
+
+	clickOurProductsButton() {
+		this.getOurProductsButton().click();
+	}
+
+	clickContactUsButton() {
+		this.getContactUsButton().click();
+	}
+}
+
+export default BasePage;
